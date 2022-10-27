a7b409a21f690c1f9a098f9b0c007a7191786bfc
antorAdhikary <antoradhikary89@gmail.com>
2022-10-27 13:52:19 +0600

    second time edit

--- js/script.js
+++ js/script.js
@@ -5,26 +5,26 @@ window.addEventListener ("scroll", function() {
 });
 
 let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+let navList = document.querySelector('.navlist');
 
 menu.onclick = () => {
 	menu.classList.toggle('bx-x');
-	navlist.classList.toggle('active');
+	navList.classList.toggle('active');
 };
 
 window.onscroll = () => {
 	menu.classList.remove('bx-x');
-	navlist.classList.remove('active');
+	navList.classList.remove('active');
 };
 
 const sr = ScrollReveal ({
 	distance: '45px',
-	duration: 2700,
+	duration: 2500,
 	reset: true
 })
 
 sr.reveal('.home-text',{delay:350, origin:'left'})
-sr.reveal('.home-img',{delay:350, origin:'right'})
+sr.reveal('.home-img',{delay:450, origin:'right'})
 
 sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:200, origin:'bottom'})
 
